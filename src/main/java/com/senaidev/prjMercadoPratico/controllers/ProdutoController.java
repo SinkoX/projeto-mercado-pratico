12c0d78c4ad0205ff809e2baf21d3f61c0887d3b
jwlioo <julio.botaccio@gmail.com>
2025-10-23 15:49:36 -0300

    Adição de metodo de id no produto

--- src/main/java/com/senaidev/prjMercadoPratico/controllers/ProdutoController.java
+++ src/main/java/com/senaidev/prjMercadoPratico/controllers/ProdutoController.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senaidev.prjMercadoPratico.dto.ProdutoDTO;
 import com.senaidev.prjMercadoPratico.entities.Produto;
 import com.senaidev.prjMercadoPratico.repositories.ProdutoRepository;
+import com.senaidev.prjMercadoPratico.services.ProdutoService;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "http://localhost:5173")
 public class ProdutoController {
 
+	 @Autowired
+	    private ProdutoService produtoService;
+	
     @Autowired
     private ProdutoRepository produtoRepository;
 
@@ -24,6 +30,10 @@ public class ProdutoController {
     @GetMapping
     public List<Produto> listarTodos() {
         return produtoRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ProdutoDTO buscarPorId(@PathVariable Long id) {
+        return produtoService.findByIdDTO(id);
     }
 
     // Buscar produtos pelo nome (parcial ou completo)
